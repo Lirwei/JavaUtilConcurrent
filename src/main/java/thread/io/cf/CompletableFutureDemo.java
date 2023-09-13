4e9be074116333829cbe36a5fca6e8731c7a1775
Lrwei <a2413044193@gmail.com>
2023-09-14 00:50:43 +0800

    CompletableFuture用法

---
+++ src/main/java/thread/io/cf/CompletableFutureDemo.java
@@ -0,0 +1,25 @@
+package thread.io.cf;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+public class CompletableFutureDemo {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        FutureTask<String> futureTask = new FutureTask<>(new MyThread());
+
+        Thread t1 = new Thread(futureTask);
+        t1.start();
+
+        System.out.println(futureTask.get());
+    }
+}
+
+
+class MyThread implements Callable<String> {
+    @Override
+    public String call() throws Exception {
+        System.out.println("--come in call() --");
+        return "hello, callable";
+    }
+}
